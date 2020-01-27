cec8a2ba4f4ecf3c13f9907051fc967901bfdeb6
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-01-27 22:02:53 +0200

    Add API route for showing the time and weather information

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+
+    /**
+     * Return the time and wather information in JSON format
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function timeAndWeather(Request $request){
+        
+    }
 }
