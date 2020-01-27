020e06d45608b68d8523d1323ac2c4998ae96c21
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-01-27 23:16:24 +0200

    Setup weather API

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -17,6 +17,38 @@ class ServiceController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function timeAndWeather(Request $request){
+        $data = []; // The data that will be send to the user
+        
+        // Data for testing
+        $lat = 30.0444;
+        $lng = 31.2357;
+
+        
+        $client = new Client();  // Create new client
+
+        // Sending a request the weather API with test data
+        $weatherRequest = $client->get('https://www.metaweather.com/api/location/search/', [
+            'query' => [
+                'lattlong'   => $lat . ',' . $lng
+            ]
+        ]);
+
+        if($weatherRequest->getStatusCode() == 200){
+            // Convert the data to object
+            $weatherData = json_decode($weatherRequest->getBody());
+            $data['weather'] = $weatherData;
+        } else {
+            return response()->json([
+                'errors'    => [
+                    'weather'   => [
+                        'Error while connecting to the weather API'
+                    ]
+                ]
+            ], 500);
+        }
+
+
+        return response()->json($data, 200);
         
     }
 }
