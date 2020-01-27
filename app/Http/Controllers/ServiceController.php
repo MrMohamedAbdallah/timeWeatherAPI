3c325c2f8ea2d162571c92f8dcffead0ac686f40
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-01-27 22:14:50 +0200

    Setup GuzzleHttp library to use it for sending requests to the external APIs

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
 class ServiceController extends Controller
 {
 
